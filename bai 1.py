713e35b93961130191948aef5bb866b1cd02a874
tuanduc123 <48420332+tuanduc123@users.noreply.github.com>
2019-03-28 16:32:56 +0700

    Add files via upload

---
+++ bai 1.py
@@ -0,0 +1,6 @@
+n1=int(input("nhap n1"))
+n2=int(input("nhap n2"))
+if n1>n2:
+    print("n1 lơn nhat")
+else:
+    print("n2 lon nhat")
---
+++ bai 10.py
@@ -0,0 +1,6 @@
+a="xin chao toi den tu Viet Nam"
+b=a.split()
+print (b)
+c=" ".\
+    join(b)
+print (c)
