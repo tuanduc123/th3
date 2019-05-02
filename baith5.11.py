6eb3a07328c7d669b68b741cd873d7eda0bae7bb
tuanduc123 <48420332+tuanduc123@users.noreply.github.com>
2019-05-02 17:32:26 +0700

    Add files via upload

---
+++ baith5.11.py
@@ -0,0 +1,10 @@
+import mymath
+
+values = [2,4,6,8,10]
+print('squares:')
+for v in values:
+    print(mymath.square(v))
+    print ('cubes:')
+for v in  values:
+    print(mymath.cube(v))
+    print('average: ' + str(mymath.average(values)))
